f2a7f4005d6b40e55e6797cefb6bdb5b6908c85c
lski <lee.cooper@lski.uk>
2017-02-05 09:03:35 +0000

    Cleaned up the length between validator

--- validators/length-between.js
+++ validators/length-between.js
@@ -1,15 +1,9 @@
 const isString = require('../utils/is-string');
-const replace = require('../utils/replace-values');
 
 module.exports = (minLength, maxLength, message = 'Should be between ' + minLength + ' and ' + maxLength + ' in length') => {
     
     return (val) => {
 
-        if(isString(val) && (val.length < minLength || val.length > maxLength)){
-
-            return [message];
-        }
-
-        return [];
+        return isString(val) && (val.length < minLength || val.length > maxLength) ? [message] : [];
     };
 };

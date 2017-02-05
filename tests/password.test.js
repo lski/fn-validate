5f5b57980f90110185210304d61ee2fb23b5ef62
lski <lee.cooper@lski.uk>
2017-02-05 09:13:36 +0000

    Added generic password validator and tests

---
+++ tests/password.test.js
@@ -0,0 +1,49 @@
+const password = require('../validators/password');
+
+test('password enforce all success', () => {
+
+    let validator = password(1, 100, "&-\\/", true, true, true, true);
+    let result = validator("ahdj0A-&")
+
+    expect(result.length).toBe(0);
+});
+
+test('password enforce length fail', () => {
+
+    let validator = password(10, 100);
+    let result = validator("ahdj0A-&")
+
+    expect(result.length).toBeGreaterThan(0);
+});
+
+test('password enforce number fail', () => {
+
+    let validator = password(10, 100, null, true, false, false, false);
+    let result = validator("asdsdkksdaksdad")
+
+    expect(result.length).toBeGreaterThan(0);
+});
+
+test('password enforce lowercase fail', () => {
+
+    let validator = password(10, 100, null, false, true, false, false);
+    let result = validator("DGJJCKD0SKKDXLX9");
+
+    expect(result.length).toBeGreaterThan(0);
+});
+
+test('password enforce uppercase fail', () => {
+
+    let validator = password(10, 100, null, false, false, true, false);
+    let result = validator("asdsdkks093daksdad");
+
+    expect(result.length).toBeGreaterThan(0);
+});
+
+test('password enforce invalid character fail', () => {
+
+    let validator = password(10, 100, null, false, false, false, false);
+    let result = validator("asdsdkks093da-0-sd)d");
+
+    expect(result.length).toBeGreaterThan(0);
+});
---
+++ validators/password.js
@@ -0,0 +1,35 @@
+const escape = require('../utils/regex-escape');
+
+module.exports = (minLength = 8, maxLength = 128, allowedSymbols = "", requireNumber = true, requireLowercase = true, requireUppercase = true, requireSymbol = false, message = "Password is not valid") => {
+
+    let regexBuilder = "^";
+    
+    if(requireNumber){
+        regexBuilder += "(?=.*\\d)";
+    }
+
+    if(requireLowercase){ 
+        regexBuilder += "(?=.*[a-z])";
+    }
+
+    if(requireUppercase){
+        regexBuilder += "(?=.*[A-Z])";
+    }
+
+    regexBuilder += "[a-zA-Z0-9";  
+    
+    if(allowedSymbols){
+        regexBuilder += escape(allowedSymbols);
+    }
+
+    regexBuilder += "]";
+
+    regexBuilder += "{" + minLength + "," + maxLength + "}$";
+
+    let regex = new RegExp(regexBuilder);
+
+    return (val) => {
+
+        return regex.test(val) ? [] : [message];
+    };
+};

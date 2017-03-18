e2d422eac86bc5606a5a267f03e884d00258d961
Lee Cooper <Lee Cooper>
2017-03-18 13:53:32 +0000

    Added generic validator and tests

---
+++ tests/generic.test.js
@@ -0,0 +1,10 @@
+const validator = require('../generic');
+
+test('generic func', () => {
+
+    var test = validator((val) => val == "hello", "a message");
+
+    expect(test("test").length).toBe(1);
+    expect(test("hello").length).toBe(0);
+    expect(test("test")[0]).toBe("a message");
+});
